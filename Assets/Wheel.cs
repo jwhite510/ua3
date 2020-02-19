d3bc1afa323485681e39b28f65eb3017a2524b91
jonathon <jonathonwhite@protonmail.com>
2020-02-19 21:47:20 +0100

    wheels move when pressing w and s

---
+++ Assets/Wheel.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Wheel : MonoBehaviour
+{
+
+    public Rigidbody wheelrigidbody;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void FixedUpdate()
+    {
+      if(Input.GetKey("w"))
+      {
+        wheelrigidbody.AddForce(Time.deltaTime*transform.right * 1000);
+        // Color color = new Color(0.0f, 0.0f, 1.0f);
+        // Debug.DrawLine(transform.position, transform.position+(Time.deltaTime*1000*transform.right), color);
+      }
+      else if(Input.GetKey("s"))
+      {
+        wheelrigidbody.AddForce(-Time.deltaTime*transform.right * 1000);
+      }
+    }
+
+
+
+}
