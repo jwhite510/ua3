ab8dfe412c76f9ace78a763c977ca104681feffa
jonathon <jonathonwhite@protonmail.com>
2020-02-27 22:51:33 +0100

    turns in a circle

--- Assets/Mech.cs
+++ Assets/Mech.cs
@@ -6,7 +6,13 @@ public class Mech : VehicleBase
 {
 
 
-    public ConfigurableJoint legjoint;
+    public ConfigurableJoint L1_leg;
+    public ConfigurableJoint L2_leg;
+    public ConfigurableJoint L3_leg;
+
+    public ConfigurableJoint R1_leg;
+    public ConfigurableJoint R2_leg;
+    public ConfigurableJoint R3_leg;
     // Start is called before the first frame update
     void Start()
     {
@@ -22,15 +28,28 @@ public class Mech : VehicleBase
     void FixedUpdate()
     {
 
-      float sinval = Mathf.Sin(0.5f*Time.time);
-      float cosval = Mathf.Cos(0.5f*Time.time);
-      sinval*=30;
-      cosval*=30;
-      Debug.Log("sinval => "+sinval);
+      float movescalar = 5.5f;
+      float sinval = Mathf.Sin(movescalar*Time.time);
+      float cosval = Mathf.Cos(movescalar*Time.time);
+      sinval*=15;
+      cosval*=15;
+      // Debug.Log("sinval => "+sinval);
 
       Quaternion e1 = new Quaternion(0,0,0,0);
       e1.eulerAngles = new Vector3(sinval,cosval,0);
-      legjoint.targetRotation = e1;
+      L1_leg.targetRotation = e1;
+      L3_leg.targetRotation = e1;
+      R1_leg.targetRotation = e1;
+      R3_leg.targetRotation = e1;
+
+      sinval = Mathf.Sin((movescalar*Time.time) + Mathf.PI);
+      cosval = Mathf.Cos((movescalar*Time.time) + Mathf.PI);
+      sinval*=15;
+      cosval*=15;
+      e1 = new Quaternion(0,0,0,0);
+      e1.eulerAngles = new Vector3(sinval,cosval,0);
+      L2_leg.targetRotation = e1;
+      R2_leg.targetRotation = e1;
 
 
     }
