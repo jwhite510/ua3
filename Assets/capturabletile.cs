4b1910593c943f20ead1e93da6ab0fbe74582766
jonathon <jonathonwhite@protonmail.com>
2020-03-01 11:48:24 +0100

    tiles switch to color of the vehicle on them

---
+++ Assets/capturabletile.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class capturabletile : MonoBehaviour
+{
+
+
+
+    private List<VehicleBase> vehicles_on_tile = new List<VehicleBase>();
+    public Renderer TileBaseRender;
+
+    Color greenc = Color.green;
+    Color redc = Color.red;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+    // Update is called once per frame
+    void Update()
+    {
+      // Debug.Log("vehicles_on_tile:");
+      int team1 = 0;
+      int team2 = 0;
+      foreach(VehicleBase veh in vehicles_on_tile)
+      {
+        // Debug.Log("veh.name => "+veh.name+" team:"+veh.team);
+        if(veh.team == 1)
+        {
+          team1++;
+        }
+        else if(veh.team == 2)
+        {
+          team2++;
+        }
+      }
+      // make weighted average
+      if(vehicles_on_tile.Count > 0)
+      {
+        Debug.Log("team1 => "+team1);
+        Debug.Log("team2 => "+team2);
+        Debug.Log("vehicles_on_tile.Count => "+vehicles_on_tile.Count);
+        float wtsum = (team1*0 + team2*1);
+        wtsum /= vehicles_on_tile.Count;
+        Debug.Log("wtsum => "+wtsum);
+        Color tilecolor = Color.Lerp(greenc, redc, wtsum);
+        TileBaseRender.GetComponent<Renderer>().material.color = tilecolor;
+      }
+
+
+    }
+    public void VehicleEnteredTile(VehicleBase vehicle)
+    {
+      // Debug.Log("capturabletile VehicleEnteredTile called");
+      // Debug.Log("vehicle.name => "+vehicle.name);
+      vehicles_on_tile.Add(vehicle);
+    }
+    public void VehicleExitTile(VehicleBase vehicle)
+    {
+      // Debug.Log("capturabletile VehicleExitTile called");
+      // Debug.Log("vehicle.name => "+vehicle.name);
+      vehicles_on_tile.Remove(vehicle);
+    }
+
+}
---
+++ Assets/triggervolume.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class triggervolume : MonoBehaviour
+{
+    public capturabletile capturabletile_o;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+      if(other.tag == "Vehicle")
+      {
+        VehicleBase vehicle = other.gameObject.GetComponentInParent<VehicleBase>();
+        if(vehicle)
+        {
+          capturabletile_o.VehicleEnteredTile(vehicle);
+        }
+      }
+    }
+    void OnTriggerExit(Collider other)
+    {
+      if(other.tag == "Vehicle")
+      {
+        VehicleBase vehicle = other.gameObject.GetComponentInParent<VehicleBase>();
+        if(vehicle)
+        {
+          capturabletile_o.VehicleExitTile(vehicle);
+        }
+      }
+    }
+}
