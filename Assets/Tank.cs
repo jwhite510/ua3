38f9e513fc5a36cfb0d2726b1ac0286e9706fc2c
jonathon <jonathonwhite@protonmail.com>
2020-03-15 13:59:46 +0100

    squad members follow squad leader

--- Assets/Tank.cs
+++ Assets/Tank.cs
@@ -74,101 +74,27 @@ public class Tank : VehicleBase
       base.FixedUpdate();
       // set tank position
       // transform.position = vehicle_base.transform.position;
-      if(MoveToWayPoint && !player_controlled)
-      {
-        // Debug.Log("GetType:"+agent.path.corners.GetType());
-        float height = 10.0f;
-        foreach(Vector3 vec in agent.path.corners)
-        {
-          // Debug.Log(vec.ToString());
-          // Debug.DrawLine(vec, vec + new Vector3(0,height,0), Color.red, 0.0f);
-          height *= 0.7f;
-        }
-
-        // Debug.DrawLine(agent.transform.position, agent.transform.position+new Vector3(0,1,0), Color.blue, 0.0f);
-
-        // drive wheels to this location
-        // agent.path.corners[0]
-        // Debug.DrawLine(
-          // vehicle_base.transform.position,
-          // vehicle_base.transform.position + 10*vehicle_base.transform.right,
-          // Color.blue,
-          // 0.0f
-            // );
-
-        // get direction from current position
-        // agent.path.corners[0] // nearest point to move to
-        // Debug.Log("Count:"+agent.path.corners.Length);
-        if(agent.path.corners.Length>1)
-        {
-          // move to this point
-          // Debug.DrawLine(
-              // agent.path.corners[1],
-              // agent.path.corners[1] + new Vector3(0.1f,10.0f,0.1f),
-              // Color.green,
-              // 0.0f
-              // );
-
-          // movement direction
-          Vector3 move_direction = agent.path.corners[1] - vehicle_base.transform.position;
-          move_direction.Normalize();
-
-          // get dot product
-          // move_direction
-          // vehicle_base.transform.right
-          float dotprod = Vector3.Dot(move_direction, vehicle_base.transform.right);
-          // Debug.Log("forwarddrive"+dotprod);
-          Vector3 crossprod = Vector3.Cross(move_direction, vehicle_base.transform.right);
-
-          // get dot product of crossprod
-          float turnvalue = Vector3.Dot(vehicle_base.transform.up, crossprod);
-
-          // Debug.Log("turn"+turnvalue);
-
-          crossprod*=10;
-
-          // draw crossprod
-          // Debug.DrawLine(
-              // vehicle_base.transform.position,
-              // vehicle_base.transform.position + crossprod,
-              // Color.green,
-              // 0.0f
-              // );
-
-
-
-          move_direction *= 10;
-          // Debug.DrawLine(
-              // vehicle_base.transform.position,
-              // vehicle_base.transform.position + move_direction,
-              // Color.green,
-              // 0.0f
-              // );
-
-
-          // drive wheels
-          if(Mathf.Abs(turnvalue)>0.5)
-          {
-            DriveWheels(-10*turnvalue, 10*turnvalue);
-          }
-          else
-          {
-            DriveWheels(10*dotprod, 10*dotprod);
-          }
-
-        }
-      }
-      // move barrel to ai at nearest enemy
       if(!player_controlled)
       {
-
         GameObject closestobject = FindNearestEnemyVehicle();
         if(closestobject)
         {
           thistankturret.autoRotateTurret(closestobject);
         }
+        // see if there is a squad leader
+        if(ui_element_name == "SquadMember")
+        {
+          // Debug.Log("move to SquadMember");
 
+          MoveToWayPoint = true;
+          Vector3 squadleaderposition = ui_element.GetComponent<SquadMemberS>().squadListItem.squadLeaderVehicle.transform.position;
+          agent.SetDestination(squadleaderposition);
 
+        }
+        if(MoveToWayPoint)
+        {
+          TankMoveToWaypoint();
+        }
       }
     }
     public void DriveWheels(float LeftSide, float RightSide)
@@ -179,5 +105,89 @@ public class Tank : VehicleBase
         RightWheel1.Drive(RightSide);
         RightWheel2.Drive(RightSide);
         RightWheel3.Drive(RightSide);
+    }
+    private void TankMoveToWaypoint()
+    {
+      // Debug.Log("GetType:"+agent.path.corners.GetType());
+      float height = 10.0f;
+      foreach(Vector3 vec in agent.path.corners)
+      {
+        // Debug.Log(vec.ToString());
+        // Debug.DrawLine(vec, vec + new Vector3(0,height,0), Color.red, 0.0f);
+        height *= 0.7f;
+      }
+
+      // Debug.DrawLine(agent.transform.position, agent.transform.position+new Vector3(0,1,0), Color.blue, 0.0f);
+
+      // drive wheels to this location
+      // agent.path.corners[0]
+      // Debug.DrawLine(
+      // vehicle_base.transform.position,
+      // vehicle_base.transform.position + 10*vehicle_base.transform.right,
+      // Color.blue,
+      // 0.0f
+      // );
+
+      // get direction from current position
+      // agent.path.corners[0] // nearest point to move to
+      // Debug.Log("Count:"+agent.path.corners.Length);
+      if(agent.path.corners.Length>1)
+      {
+        // move to this point
+        // Debug.DrawLine(
+        // agent.path.corners[1],
+        // agent.path.corners[1] + new Vector3(0.1f,10.0f,0.1f),
+        // Color.green,
+        // 0.0f
+        // );
+
+        // movement direction
+        Vector3 move_direction = agent.path.corners[1] - vehicle_base.transform.position;
+        move_direction.Normalize();
+
+        // get dot product
+        // move_direction
+        // vehicle_base.transform.right
+        float dotprod = Vector3.Dot(move_direction, vehicle_base.transform.right);
+        // Debug.Log("forwarddrive"+dotprod);
+        Vector3 crossprod = Vector3.Cross(move_direction, vehicle_base.transform.right);
+
+        // get dot product of crossprod
+        float turnvalue = Vector3.Dot(vehicle_base.transform.up, crossprod);
+
+        // Debug.Log("turn"+turnvalue);
+
+        crossprod*=10;
+
+        // draw crossprod
+        // Debug.DrawLine(
+        // vehicle_base.transform.position,
+        // vehicle_base.transform.position + crossprod,
+        // Color.green,
+        // 0.0f
+        // );
+
+
+
+        move_direction *= 10;
+        // Debug.DrawLine(
+        // vehicle_base.transform.position,
+        // vehicle_base.transform.position + move_direction,
+        // Color.green,
+        // 0.0f
+        // );
+
+
+        // drive wheels
+        if(Mathf.Abs(turnvalue)>0.5)
+        {
+          DriveWheels(-10*turnvalue, 10*turnvalue);
+        }
+        else
+        {
+          DriveWheels(10*dotprod, 10*dotprod);
+        }
+
+      }
     }
 }
